00f298fc3c06a3217c047db7937df3e579d62824
jailton-silvasp <131924910+jailton-silvasp@users.noreply.github.com>
2026-05-06 12:13:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const { Pool } = require("pg");
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-
+const express = require('express')
+const cors = require('cors')
 const { Pool } = require('pg')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
